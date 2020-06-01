e49b2bf76a4d569b910bb54a45990ea769f54f1b
dw-j <wittner.dominic@gmail.com>
2020-06-01 20:31:35 +0200

    added taskstuff

--- gui/components.py
+++ gui/components.py
@@ -2,28 +2,38 @@ from tkinter import *
 import tkinter.font as tkFont
 
 class DComponent:
-    def __init__(self, root, name, x_position, y_position, width, height, pad_x, pad_y, fontsize):
+    def __init__(self, name, root, x_position, y_position, width, height, fontsize):
         self.root = root
         self.name = name
-        self.x_position = x_position
-        self.y_position = y_position
-        self.width = width
-        self.height = height
-        self.pad_x = pad_x
-        self.pad_y = pad_y
+        self.x_position, self.y_position = x_position, y_position
+        self.width, self.height = width, height
+        self.stick = 'wns'
+        self.pad_x, self.pad_y = 0, 0
         if fontsize != None: self.font = tkFont.Font(family="Times New Roman", size=fontsize)
 
 class DPlaceHolder(DComponent):
     def __init__(self, root, x_position, y_position):
-        super().__init__(root, None, x_position, y_position, 1, 1, 0, 0, None)
-        self.component = Label(root)
+        super().__init__(root, None, x_position, y_position, 1, 1, None)
+        self.component = Label(root, text='test')
 
 class DLabel(DComponent):
-    def __init__(self, name, root, text, x_position, y_position, width, height, pad_x, pad_y, fontsize):
-        super().__init__(root, name, x_position, y_position, width, height, pad_x, pad_y, fontsize)
+    def __init__(self, name, root, x_position, y_position, width, height, pad_x, pad_y, fontsize, text):
+        super().__init__(name, root, x_position, y_position, width, height, fontsize)
+        self.pad_x, self.pad_y = pad_x, pad_y
         self.component = Label(root, text=text, font=self.font, justify=LEFT)
 
 class DCanvas(DComponent):
     def __init__(self, name, root, width, height):
-        super().__init__(root, name, 0, 0, width, height, 0, 0, None)
+        super().__init__(name, root, 0, 0, width, height, 0, 0, None)
         self.component = Canvas(root, width=width, height=height)
+
+class DEntry(DComponent):
+    def __init__(self, name,  root, x_position, y_position, width, height, fontsize, state):
+        super().__init__(name, root, x_position, y_position, width, height, fontsize)
+        self.component = Entry(root, state = state, font=self.font)
+
+class DButton(DComponent):
+    def __init__(self, name, root, x_position, y_position, width, height, fontsize, text, command, stick):
+        super().__init__(name, root, x_position, y_position, width, height, fontsize)
+        self.stick = stick
+        self.component = Button(root, text=text, command=command, font=self.font)
--- gui/frame.py
+++ gui/frame.py
@@ -1,5 +1,6 @@
 import tkinter 
-from gui.components import DLabel, DCanvas, DPlaceHolder
+from gui.components import DLabel, DCanvas, DPlaceHolder, DEntry, DButton
+from tkinter import DISABLED, NORMAL
 
 class DFrame:
     def __init__(self, title):
@@ -16,22 +17,26 @@ class DFrame:
         elements = []
 
         #add placeholderrs
-        for i in range(0, 15):
-            elements.append(DPlaceHolder(self.root, i, 0))
-            elements.append(DPlaceHolder(self.root, 0, i))
-            self.root.grid_columnconfigure(i, weight=5)
-            self.root.grid_rowconfigure(i, weight=2)
-
+        for i in range(0, 50):
+            #elements.append(DPlaceHolder(self.root, i, 0))
+            #elements.append(DPlaceHolder(self.root, 0, i))
+            self.root.grid_columnconfigure(i, weight=1)
+            self.root.grid_rowconfigure(i, weight=1)
+    
         #add title and canvas
         #elements.append(DCanvas('canvas', self.root, w, h))
-        elements.append(DLabel('title', self.root, 'Scheduler', 0, 0, 1, 1, 20, 20, 40))
+        elements.append(DLabel('title', self.root, 0, 0, 1, 1, 20, 20, 40, 'Scheduler'))
 
         #add task edit
-        elements.append(DLabel('task_label', self.root, 'Task', 11, 1, 1, 1, 0, 0, 28))
+        column, row = 40, 10
+        
         fields = ['ID', 'Task', 'Day', 'From', 'To', 'Color']
+        elements.append(DLabel('task_label', self.root, column, row-1, 1, 1, 0, 0, 28, 'Task'))
         for index, field in enumerate(fields):
-            elements.append(DLabel('{}_label'.format(field.lower()), self.root, field, 11, 2+index, 1, 1, 0, 0, 22))
-
+            elements.append(DLabel('{}_label'.format(field.lower()), self.root, column, row+2*index, 1, 1, 0, 0, 22, field))
+            elements.append(DEntry('{}_text'.format(field.lower()), self.root, column+1, row+2*index, 1, 1, 22, DISABLED if field == 'ID' else NORMAL))
+        elements.append(DButton('add_button', self.root, column, row+1+len(fields)*2, 2,1, 22, 'Add', None, 'snwe'))
+        elements.append(DButton('remove_button', self.root, column, row+3+len(fields)*2, 2,1, 22, 'Remove', None, 'snwe'))
         return elements
     
     def add_component(self, element):
@@ -41,7 +46,7 @@ class DFrame:
                                 rowspan=element.height,
                                 padx=element.pad_x,
                                 pady=element.pad_y,
-                                sticky='w')
+                                sticky=element.stick)
         
     def show(self):
         for element in self.elements:
